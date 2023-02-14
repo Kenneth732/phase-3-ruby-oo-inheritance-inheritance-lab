c16ad872cf6726af84ebb5e708e5fff13870fa47
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-02-14 15:47:10 +0300

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative 'lib/user'
 
+class Student < User
+  attr_reader :knowledge
+
+  def initialize(first_name, last_name)
+    super
+    @knowledge = []
+  end
+
+  def learn(info)
+    @knowledge << info
+  end
 end
